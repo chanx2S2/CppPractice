4233beba0f3c1984d8d9da89483c0e02f4eb5be3
chanx2S2 <cksdud285@gmail.com>
2023-10-04 21:12:06 +0900

    cpp04+cpp05_finished

---
+++ cpp04/cpp04/cpp04.cpp
@@ -0,0 +1,137 @@
+﻿/*
+
+// 1. Caesar cipher
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    int n;
+
+    cout << "Input string: ";
+    cin >> str;
+
+    cout << "Input key: ";
+    cin >> n;
+
+    cout << "Cipher string: ";
+    cout << char(str[0] + n);
+    cout << char(str[1] + n);
+    cout << char(str[2] + n);
+    cout << char(str[3] + n);
+    cout << char(str[4] + n);
+    cout << char(str[5] + n);
+    cout << char(str[6] + n);
+    cout << char(str[7] + n);
+
+}
+
+*/
+
+/*
+
+// 2. Decryption
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    int n;
+
+    cout << "Input string: ";
+    cin >> str;
+
+    cout << "Input key: ";
+    cin >> n;
+
+    cout << "Cipher string: ";
+    cout << char(str[0] - n);
+    cout << char(str[1] - n);
+    cout << char(str[2] - n);
+    cout << char(str[3] - n);
+    cout << char(str[4] - n);
+    cout << char(str[5] - n);
+    cout << char(str[6] - n);
+    cout << char(str[7] - n);
+
+}
+
+*/
+
+/*
+
+// 3. Quadrant
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    string str;
+
+    cout << "Input x: ";
+    cin >> a;
+
+    cout << "Input y: ";
+    cin >> b;
+
+    (a > 0 && b > 0) ? str = "It is located in the 1st quadrant.": "";
+    (a < 0 && b > 0) ? str = "It is located in the 2nd quadrant." : "";
+    (a < 0 && b < 0) ? str = "It is located in the 3rd quadrant." : "";
+    (a > 0 && b < 0) ? str = "It is located in the 4th quadrant." : "";
+    (a == 0 && b == 0) ? str = "It is origin point." : "";
+
+    cout << str;
+
+}
+
+*/
+
+/*
+
+// 4. Population
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long a, b;
+
+    cout << "Enter the world's population: ";
+    cin >> a;
+
+    cout << "Enter the population of KOR: ";
+    cin >> b;
+
+    cout << "The population of KOR is " << ((double) b / a) << "% of the wold population.";
+
+}
+
+*/
+
+// 5. Your height
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double h;
+
+    cout << "Input your height: ";
+    cin >> h;
+
+    cout << h << "cm is " << (int) (h / 30.48) << " feet " << (h - (int)(h / 30.48) * 30.48) / 2.54 << " inch.";
+
+}
---
+++ cpp05/cpp05/cpp05.cpp
@@ -0,0 +1,190 @@
+﻿
+/*
+
+// 1. Advanced calculator
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char op;
+    int a;
+    int b;
+
+    cout << "--------------" << endl;
+    cout << "+ : add" << endl;
+    cout << "- : subtract" << endl;
+    cout << "* : multiply" << endl;
+    cout << "/ : divide" << endl;
+    cout << "Q : quit" << endl;
+    cout << "---------------" << endl;
+
+    cout << "Select an operator: ";
+    
+    cin >> op;
+
+    if (op == 'Q') return 0;
+
+    cout << "Input two number: ";
+    cin >> a;
+    cin >> b;
+
+    cout << "Answer: ";
+
+    if (op == '+') cout << a + b;
+    else if (op == '-') cout << a - b;
+    else if (op == '*') cout << a * b;
+    else if (op == '/') {
+        if (b == 0) cout << "Cannot divide by 0.";
+        else cout << a / b;
+    }
+}
+
+*/
+
+/*
+
+// 2. Discount
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int item;
+
+    cout << "number of items? ";
+    cin >> item;
+
+    cout << "price: ";
+    
+    if (item >= 10) {
+        cout << item * 1000 << " KRW" << endl;
+        cout << "discounted price: ";
+        cout << (item * 1000) - (item * 1000 * 0.1) << " KRW";
+    }
+    else cout << item * 1000 << " KRW";
+        
+}
+
+*/
+
+/*
+
+// 3. Digits two
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    cout << "Input your number: ";
+
+    int num;
+    cin >> num;
+
+    if (num < 10000) {
+        
+        int thousands = num / 1000;
+
+        cout << "thousands: ";
+
+        if (thousands == 0) cout << "zero" << endl;
+        if (thousands == 1) cout << "one" << endl;
+        if (thousands == 2) cout << "two" << endl;
+        if (thousands == 3) cout << "three" << endl;
+        if (thousands == 4) cout << "four" << endl;
+        if (thousands == 5) cout << "five" << endl;
+        if (thousands == 6) cout << "six" << endl;
+        if (thousands == 7) cout << "seven" << endl;
+        if (thousands == 8) cout << "eight" << endl;
+        if (thousands == 9) cout << "nine" << endl;
+
+        int hundreds = (num % 1000) / 100;
+
+        cout << "hundreds: ";
+
+        if (hundreds == 0) cout << "zero" << endl;
+        if (hundreds == 1) cout << "one" << endl;
+        if (hundreds == 2) cout << "two" << endl;
+        if (hundreds == 3) cout << "three" << endl;
+        if (hundreds == 4) cout << "four" << endl;
+        if (hundreds == 5) cout << "five" << endl;
+        if (hundreds == 6) cout << "six" << endl;
+        if (hundreds == 7) cout << "seven" << endl;
+        if (hundreds == 8) cout << "eight" << endl;
+        if (hundreds == 9) cout << "nine" << endl;
+
+        int tens = ((num % 1000) % 100) / 10;
+
+        cout << "tens: ";
+
+        if (tens == 0) cout << "zero" << endl;
+        if (tens == 1) cout << "one" << endl;
+        if (tens == 2) cout << "two" << endl;
+        if (tens == 3) cout << "three" << endl;
+        if (tens == 4) cout << "four" << endl;
+        if (tens == 5) cout << "five" << endl;
+        if (tens == 6) cout << "six" << endl;
+        if (tens == 7) cout << "seven" << endl;
+        if (tens == 8) cout << "eight" << endl;
+        if (tens == 9) cout << "nine" << endl;
+
+        int ones = ((num % 1000) % 100) % 10;
+
+        cout << "ones: ";
+
+        if (ones == 0) cout << "zero" << endl;
+        if (ones == 1) cout << "one" << endl;
+        if (ones == 2) cout << "two" << endl;
+        if (ones == 3) cout << "three" << endl;
+        if (ones == 4) cout << "four" << endl;
+        if (ones == 5) cout << "five" << endl;
+        if (ones == 6) cout << "six" << endl;
+        if (ones == 7) cout << "seven" << endl;
+        if (ones == 8) cout << "eight" << endl;
+        if (ones == 9) cout << "nine" << endl;
+
+    } else return 0;
+}
+
+*/
+
+// 4. Minimum values
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+
+    cout << "input numbers:  ";
+    cin >> a;
+    cin >> b;
+    cin >> c;
+
+    cout << "minimum number: ";
+
+    if (a < b) {
+        if (a < c) cout << a;
+        else if (c < a) cout << c;
+    }
+    else if (b < c) {
+        if (b < a) cout << b;
+        else if (a < b) cout << a;
+    }
+
+    else if (c < a) {
+        if (c < b) cout << c;
+        else if (b < c) cout << b;
+    }
+
+}
+
