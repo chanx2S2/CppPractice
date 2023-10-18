8d0c48ce0ffccb89f839af33fb2195d5e02ae377
chanx2S2 <cksdud285@gmail.com>
2023-10-18 14:26:14 +0900

    ~cpp07 finished

---
+++ cpp06/cpp06/cpp06.cpp
@@ -0,0 +1,193 @@
+﻿
+/*
+
+// 1. Order list
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int menu;
+    int bugger = 0;
+    int fries = 0;
+    int coke = 0;
+    
+    while (1) {
+
+        cout << "Select a menue (1-4)" << endl;
+        cout << "1) Bugger, 2) Fries, 3) Coke, 4) Finish" << endl;
+        cin >> menu;
+
+        if (menu == 1) {
+            cout << "You've ordered a Bugger\n" << endl;
+            bugger++;
+        }
+
+        else if (menu == 2) {
+            cout << "You've ordered a Fries\n" << endl;
+            fries++;
+        }
+
+        else if (menu == 3) {
+            cout << "You've ordered a Coke\n" << endl;
+            coke++;
+        }
+
+        else if (menu == 4) {
+            cout << "Your order has been received!\n" << endl;
+            break;
+        }
+
+    }
+
+    cout << "Your orders" << endl;
+
+    if (bugger > 0) {
+        cout << "1) Bugger * " << bugger << endl;
+    }
+
+    if (fries > 0) {
+        cout << "2) Fries * " << fries << endl;
+    }
+
+    if (coke > 0) {
+        cout << "3) Coke * " << coke << endl;
+    }
+}
+
+// 2. Times Table
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i < 10; i++) {
+        for (int j = 2; j < 10; j++) {
+            cout << j << "*" << i << "=" << j * i << "  ";
+        }
+        cout << endl;
+    }
+}
+
+*/
+
+// 3. Lotto
+
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+int main()
+{
+    random_device rd;
+    mt19937 gen(rd());
+
+    int a, b, c, d, e, f;
+
+    while(1) {
+        a = (gen() % 45);
+        b = (gen() % 45);
+        c = (gen() % 45);
+        d = (gen() % 45);
+        e = (gen() % 45);
+        f = (gen() % 45);
+
+        if (a == b || a == c || a == d || a == e || a == f) continue;
+        if (b == c || b == d || b == e || b == f) continue;
+        if (c == d || c == e || c == f) continue;
+        if (d == e || d == f) continue;
+        if (e == f) continue;
+        else break;
+
+    }
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << endl;
+    cout << e << endl;
+    cout << f << endl;
+}
+
+/*
+
+// 4. Diamond
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int height;
+    
+    cout << "input height: ";
+    cin >> height;
+
+    if (height % 2 == 0) {
+        cout << "Only ODD numbers are allowed!" << endl;
+        return 0;
+    }
+
+    for (int i = 0; i < height; i++) {
+
+        if (i % 2 == 0) {
+            for (int j = height - 1; j > i; j = j - 2) {
+                cout << " ";
+            }
+            for (int j = 0; j <= i; j++) {
+                cout << "*";
+            }
+            cout << endl;
+        }
+    }
+
+    for (int i = 0; i < height-1; i++) {
+
+        if (i % 2 == 0) {
+            for (int j = 0; j <= i; j = j + 2) {
+                cout << " ";
+            }
+            for (int j = height-2; j > i; j--) {
+                cout << "*";
+            }
+            cout << endl;
+        }
+    }
+}
+
+// 5. Pascal's triangle
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int height;
+
+    cout << "input height: ";
+    cin >> height;
+
+    for (int i = 0; i < height; i++) {
+
+        int num = 1;
+        
+        for (int j = 1; j < (height - i); j++) {
+            cout << " ";
+        }
+        for (int k = 0; k <= i; k++) {
+            cout << " " << num;
+            num = num * (i - k) / (k + 1);
+        }
+        cout << endl;
+
+    }
+}
+
+*/
---
+++ cpp07/cpp07/cpp07.cpp
@@ -0,0 +1,275 @@
+﻿
+/*
+
+// 1. Lotto game!
+
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+void lottery(int a, int b, int c, int d, int e, int f);
+
+int main()
+{
+
+    int a, b, c, d, e, f;
+
+    cout << "Guess lottery number:" << endl;
+
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+    cin >> e;
+    cin >> f;
+
+    cout << endl;
+    
+    lottery(a, b, c, d, e, f);
+
+}
+
+void lottery(int a, int b, int c, int d, int e, int f) {
+
+    random_device rd;
+    mt19937 gen(rd());
+
+    int a2, b2, c2, d2, e2, f2;
+    int count = 0;
+
+    cout << "Number: ";
+
+    while (1) {
+
+        a2 = gen() % 45 + 1;
+        b2 = gen() % 45 + 1;
+        c2 = gen() % 45 + 1;
+        d2 = gen() % 45 + 1;
+        e2 = gen() % 45 + 1;
+        f2 = gen() % 45 + 1;
+
+        if (a2 == b2 || a2 == c2 || a2 == d2 || a2 == e2 || a2 == f2) continue;
+        if (b2 == c2 || b2 == d2 || b2 == e2 || b2 == f2) continue;
+        if (c2 == d2 || c2 == e2 || c2 == f2) continue;
+        if (d2 == e2 || d2 == f2) continue;
+        if (e2 == f2) continue;
+        else break;
+    }
+
+    cout << a2 << " " << b2 << " " << c2 << " "
+         << d2 << " " << e2 << " " << f2 << endl;
+    cout << endl;
+
+    if (a == a2) count++;
+    if (b == b2) count++;
+    if (c == c2) count++;
+    if (d == d2) count++;
+    if (e == e2) count++;
+    if (f == f2) count++;
+
+    cout << "You match " << count << " numbers! ";
+
+    if (count == 3) cout << "You are in 5th place!";
+    else if (count == 4) cout << "You are in 4th place!";
+    else if (count == 5) cout << "You are in 3th or 2th place!";
+    else if (count == 6) cout << "You are in 1th place!";
+    else cout << "Try again!";
+
+}
+
+// 2. Prime number
+
+#include <iostream>
+
+using namespace std;
+
+bool isPrime(int n);
+
+int main()
+{
+    int num;
+    
+    while (1) {
+
+        cout << "input number: ";
+        cin >> num;
+
+        if (num == -1) {
+            cout << "Done." << endl;
+            break;
+        }
+        else if (isPrime(num)) {
+            cout << num << " is a prime number." << endl;
+            cout << endl;
+        }
+        else {
+            cout << num << " is not a prime number." << endl;
+            cout << endl;
+        }
+
+    }
+}
+
+bool isPrime(int n) {
+
+    for (int i = 2; (i * i) <= n; i++) {
+        if ((n % i) == 0) {
+            return 0;
+        }
+    }
+    
+    return 1;
+}
+
+// 3. ipower()
+
+#include <iostream>
+
+using namespace std;
+
+int ipower(int n, int k);
+
+int main()
+{
+    int num;
+
+    cout << "input number: ";
+    cin >> num;
+
+    for (int i = 0; i < 11; i++) {
+        cout << num << "^" << i << " = " << ipower(num, i) << endl;
+    }
+
+}
+
+int ipower(int n, int k) {
+    
+    int mul = n;
+
+    if (k == 0) return 1;
+
+    for (int i = 1; i < k; i++) {
+        mul = mul * n;
+    }
+
+    return mul;
+}
+
+*/
+
+// 4. Probability
+
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+void graph(int len, int dot);
+
+int main()
+{
+    
+    int num;
+
+    cout << "Roll a dice 'n' times: ";
+    cin >> num;
+    
+    graph(num, 20);
+    
+}
+
+void graph(int len, int dot) {
+
+    random_device rd;
+    mt19937 gen(rd());
+    
+    int tmp = 0;
+    int a = 0, b = 0, c = 0,
+        d = 0, e = 0, f = 0;
+    
+
+    for (int i = 0; i < len; i++) {
+        tmp = gen() % 6 + 1;
+
+        if (tmp == 1) a++;
+        else if (tmp == 2) b++;
+        else if (tmp == 3) c++;
+        else if (tmp == 4) d++;
+        else if (tmp == 5) e++;
+        else if (tmp == 6) f++;
+
+        tmp = 0;
+    }
+
+
+    cout << endl;
+    /*
+    cout << "\n\n" << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << endl;
+    cout << e << endl;
+    cout << f << endl;
+    */
+
+    // 주사위 1 그래프
+    cout << "1:";
+    for (int i = 0; i < a; i++) {
+        cout << "@";
+    }
+    for (int i = 0; i < (len-a); i++) {
+        cout << "*";
+    }
+    cout << endl;
+
+    // 주사위 2 그래프
+    cout << "2:";
+    for (int i = 0; i < b; i++) {
+        cout << "@";
+    }
+    for (int i = 0; i < (len - b); i++) {
+        cout << "*";
+    }
+    cout << endl;
+
+    // 주사위 3 그래프
+    cout << "3:";
+    for (int i = 0; i < c; i++) {
+        cout << "@";
+    }
+    for (int i = 0; i < (len - c); i++) {
+        cout << "*";
+    }
+    cout << endl;
+
+    // 주사위 4 그래프
+    cout << "4:";
+    for (int i = 0; i < d; i++) {
+        cout << "@";
+    }
+    for (int i = 0; i < (len - d); i++) {
+        cout << "*";
+    }
+    cout << endl;
+
+    // 주사위 5 그래프
+    cout << "5:";
+    for (int i = 0; i < e; i++) {
+        cout << "@";
+    }
+    for (int i = 0; i < (len - e); i++) {
+        cout << "*";
+    }
+    cout << endl;
+
+    // 주사위 6 그래프
+    cout << "6:";
+    for (int i = 0; i < f; i++) {
+        cout << "@";
+    }
+    for (int i = 0; i < (len - f); i++) {
+        cout << "*";
+    }
+    cout << endl;
+}
